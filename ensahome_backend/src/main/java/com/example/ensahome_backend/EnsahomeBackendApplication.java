e775760ccd4a424d5f56813a66d8c71d486f7c1f
imassenda <salmaimassenda2023@gmail.com>
2025-05-08 23:36:30 +0100

    set up backend & frontend architecture

---
+++ ensahome_backend/src/main/java/com/example/ensahome_backend/EnsahomeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ensahome_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsahomeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnsahomeBackendApplication.class, args);
+    }
+
+}
