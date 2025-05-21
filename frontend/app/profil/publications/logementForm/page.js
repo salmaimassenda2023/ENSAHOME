35f4f9e63f300cf9f0f7318bc4ab9d02347597e2
imassenda <salmaimassenda2023@gmail.com>
2025-05-21 22:21:49 +0100

    Publier des logements UI

--- frontend/app/profil/publications/logementForm/page.js
+++ frontend/app/profil/publications/logementForm/page.js
@@ -267,16 +267,26 @@ export default function LogementForm() {
                                     {errors.loyer && <p className="text-red-500 text-sm mt-1">{errors.loyer}</p>}
                                 </div>
 
-                                {/* photos */}
+                                {/* Photos */}
                                 <div>
-                                    <label className="block text-xl font-medium text-gray-700 leading-tight">Photos du logement :</label>
-                                    <input
-                                        type="file"
-                                        multiple
-                                        accept="image/*"
-                                        onChange={handlePhotoUpload}
-                                        className={`mt-2 block w-full h-10 rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 ${errors.photos ? 'border-red-500' : ''}`}
-                                    />
+                                    <label className="block text-xl font-medium text-gray-700 leading-tight">Photos de l'équipement :</label>
+                                    <div className="mt-2 flex items-center justify-center w-full">
+                                        <label className="flex flex-col items-center justify-center w-full h-10 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+                                            <div className="flex items-center justify-center">
+                                                <svg className="w-4 h-4 mr-2 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                                                </svg>
+                                                <span className="text-sm text-gray-500">Téléchargement d'une pièce jointe</span>
+                                            </div>
+                                            <input
+                                                type="file"
+                                                multiple
+                                                accept="image/*"
+                                                onChange={handlePhotoUpload}
+                                                className="hidden"
+                                            />
+                                        </label>
+                                    </div>
                                     {errors.photos && <p className="text-red-500 text-sm mt-1">{errors.photos}</p>}
                                     {formData.photos.length > 0 && (
                                         <p className="text-sm text-gray-600 mt-1">
